bbfb507d9fe1b926a0d7017f797c0b64af4cfcb6
Adrian-George Bostan <adrg@epistack.com>
2020-10-14 11:27:30 +0300

    Add GoDoc package documentation

--- frontmatter.go
+++ frontmatter.go
@@ -1,3 +1,23 @@
+/*
+Package frontmatter implements detection and decoding for various content
+front matter formats.
+
+  The following front matter formats are supported by default.
+
+  - YAML identified by:
+    • opening and closing `---` lines.
+    • opening `---yaml` and closing `---` lines.
+  - TOML identified by:
+    • opening and closing `+++` lines.
+    • opening `---toml` and closing `---` lines.
+  - JSON identified by:
+    • opening and closing `;;;` lines.
+    • opening `---json` and closing `---` lines.
+    • a single JSON object followed by an empty line.
+
+If the default formats are not suitable for your use case, you can easily bring
+your own. See the examples for more information.
+*/
 package frontmatter
 
 import (
@@ -9,19 +29,6 @@ import (
 // is not present, the original data is returned and `v` is left unchanged.
 // Front matters are detected and decoded based on the passed in `formats`.
 // If no formats are provided, the default formats are used.
-//
-// The following front matter formats are supported by default.
-//
-//  - YAML identified by:
-//    • opening and closing `---` lines.
-//    • opening `---yaml` and closing `---` lines.
-//  - TOML identified by:
-//    • opening and closing `+++` lines.
-//    • opening `---toml` and closing `---` lines.
-//  - JSON identified by:
-//    • opening and closing `;;;` lines.
-//    • opening `---json` and closing `---` lines.
-//    • a single JSON object followed by an empty line.
 func Parse(r io.Reader, v interface{}, formats ...*Format) ([]byte, error) {
 	if len(formats) == 0 {
 		formats = defaultFormats()
