b068fc8fddad6fcfec788f0b66d0430ff801d24f
Adrian-George Bostan <adrg@epistack.com>
2020-10-14 11:27:50 +0300

    Add GoDoc examples

---
+++ example_test.go
@@ -0,0 +1,118 @@
+package frontmatter_test
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/adrg/frontmatter"
+	"gopkg.in/yaml.v2"
+)
+
+func ExampleParse_yAML() {
+	r := strings.NewReader(`
+---
+name: "frontmatter"
+tags: ["go", "yaml", "json", "toml"]
+---
+rest of the content`)
+
+	var matter struct {
+		Name string   `yaml:"name" toml:"name" json:"name"`
+		Tags []string `yaml:"tags" toml:"tags" json:"tags"`
+	}
+
+	rest, err := frontmatter.Parse(r, &matter)
+	if err != nil {
+		// Treat error.
+	}
+
+	fmt.Printf("%+v\n", matter)
+	fmt.Println(string(rest))
+
+	// Output:
+	// {Name:frontmatter Tags:[go yaml json toml]}
+	// rest of the content
+}
+
+func ExampleParse_tOML() {
+	r := strings.NewReader(`
++++
+name = "frontmatter"
+tags = ["go", "yaml", "json", "toml"]
++++
+rest of the content`)
+
+	var matter struct {
+		Name string   `yaml:"name" toml:"name" json:"name"`
+		Tags []string `yaml:"tags" toml:"tags" json:"tags"`
+	}
+
+	rest, err := frontmatter.Parse(r, &matter)
+	if err != nil {
+		// Treat error.
+	}
+
+	fmt.Printf("%+v\n", matter)
+	fmt.Println(string(rest))
+
+	// Output:
+	// {Name:frontmatter Tags:[go yaml json toml]}
+	// rest of the content
+}
+
+func ExampleParse_jSON() {
+	r := strings.NewReader(`
+{
+  "name": "frontmatter",
+  "tags": ["go", "yaml", "json", "toml"]
+}
+
+rest of the content`)
+
+	var matter struct {
+		Name string   `yaml:"name" toml:"name" json:"name"`
+		Tags []string `yaml:"tags" toml:"tags" json:"tags"`
+	}
+
+	rest, err := frontmatter.Parse(r, &matter)
+	if err != nil {
+		// Treat error.
+	}
+
+	fmt.Printf("%+v\n", matter)
+	fmt.Println(string(rest))
+
+	// Output:
+	// {Name:frontmatter Tags:[go yaml json toml]}
+	// rest of the content
+}
+
+func ExampleParse_custom() {
+	r := strings.NewReader(`
+...
+name: "frontmatter"
+tags: ["go", "yaml", "json", "toml"]
+...
+rest of the content`)
+
+	var matter struct {
+		Name string   `yaml:"name"`
+		Tags []string `yaml:"tags"`
+	}
+
+	formats := []*frontmatter.Format{
+		frontmatter.NewFormat("...", "...", yaml.Unmarshal),
+	}
+
+	rest, err := frontmatter.Parse(r, &matter, formats...)
+	if err != nil {
+		// Treat error.
+	}
+
+	fmt.Printf("%+v\n", matter)
+	fmt.Println(string(rest))
+
+	// Output:
+	// {Name:frontmatter Tags:[go yaml json toml]}
+	// rest of the content
+}
